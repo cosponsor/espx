38bc2d598a2b2827b581b7dd4acf962133c73ca1
south <68041302+iuml@users.noreply.github.com>
2024-08-05 23:05:06 -0600

    Update and rename x4.lua to x5.lua

--- x5.lua
+++ x5.lua
@@ -89,7 +89,7 @@ local function createEsp(player)
         distance = create("Text", {
             Color = Color3.new(1, 1, 1),
             Size = 12,
-            Font = 1,
+            Font = 2,
             Outline = true,
             Center = true
         }),
